838b06effb28a0896e1fa216988d0d6bac4009c8
evykassirer <evy.kassirer@gmail.com>
2017-12-31 23:14:19 -0500

    very baseline stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.gif';
 import './App.css';
+
+// TODO : set up database
+import warmups from './warmups.json';
 
 class App extends Component {
   render() {
+    const warmupList = warmups["warmups"];
+    const warmupComponents = warmupList.map(
+      warmup => <Warmup key={warmup.name} data={warmup}/>);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Choir Warmups</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {warmupComponents}
       </div>
     );
+  }
+}
+
+class Warmup extends Component {
+  render() {
+    const data = this.props.data;
+    return <div>{data.description}</div>;
   }
 }
 
