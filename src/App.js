382b643686fa1d68381d47ccb3e2886a20675de4
evykassirer <evy.kassirer@gmail.com>
2018-02-18 19:12:24 -0800

    separate logic in App.js into separate functions

--- src/App.js
+++ src/App.js
@@ -18,11 +18,13 @@ class App extends Component {
     selectedFilterTags: [],
   }
 
-  tagSearchChanged = (selected) => {
-    this.setState({selectedFilterTags: selected});
+
+  tagSearchChanged = (selectedTags) => {
+    this.setState({selectedFilterTags: selectedTags});
   }
 
-  render() {
+
+  sortedWarmupList() {
     const warmupList = warmups["warmups"];
     warmupList.sort(function(a, b) {
         if (a.name.toLowerCase() < b.name.toLowerCase()) {
@@ -33,31 +35,79 @@ class App extends Component {
         }
         return 0;
     });
+    return warmupList;
+  }
 
+
+  sortedFilteredWarmupList() {
+    return this.sortedWarmupList().filter(warmup => {
+      if (!warmup.name.includes(this.state.warmupFilterText)) {
+        return false;
+      }
+
+      for (const tag of this.state.selectedFilterTags) {
+        if (!warmup.tags.includes(tag.value)) {
+          return false;
+        }
+      }
+
+      return true;
+    });
+  }
+
+
+  tagsList() {
+    const warmupList = warmups["warmups"];
     const tags = new Set();
     for (const warmup of warmupList) {
       warmup.tags.forEach(t => tags.add(t));
     }
+    return Array.from(tags);
+  }
 
+
+  renderFilters() {
+    return <div className="App-search">
+      <AutoComplete
+        floatingLabelText="Search by warmup name"
+        filter={AutoComplete.fuzzyFilter}
+        dataSource={this.sortedWarmupList().map(w => w.name)}
+        maxSearchResults={3}
+        fullWidth
+        onUpdateInput={text => this.setState({warmupFilterText: text})}
+      />
+
+      <Select
+        name="tag-search-field"
+        placeholder="Filter by tags"
+        value={this.state.selectedFilterTags}
+        onChange={this.tagSearchChanged}
+        multi={true}
+        options={this.tagsList().map(t => {
+          return {value: t, label: t};
+        })}
+      />
+    </div>;
+  }
+
+
+  renderViewSource() {
+    return <div className="view-source">
+      <img src={githubIcon} className="github-icon" alt="github icon"/>
+      <a
+        href="https://github.com/evykassirer/choir-warmups"
+        className="view-source-link">
+        View source code on GitHub
+      </a>
+    </div>
+  }
+
+  render() {
     const warmupComponents = [];
-    for (const warmup of warmupList) {
-      if (!warmup.name.includes(this.state.warmupFilterText)) {
-        continue;
-      }
-      if (this.state.selectedFilterTags.length > 0) {
-        let tagsMatch = true;
-        for (const tag of this.state.selectedFilterTags) {
-          if (!warmup.tags.includes(tag.value)) {
-            tagsMatch = false;
-          }
-        }
-        if (!tagsMatch) {
-          continue;
-        }
-      }
+    this.sortedFilteredWarmupList().forEach(warmup => {
       warmupComponents.push(<Warmup key={warmup.name} data={warmup}/>);
       warmupComponents.push(<Divider/>);
-    }
+    });
 
     return <MuiThemeProvider>
       <div className="App">
@@ -65,27 +115,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Choir Warmups</h1>
         </header>
-        <div className="App-search">
-          <AutoComplete
-            floatingLabelText="Search by warmup name"
-            filter={AutoComplete.fuzzyFilter}
-            dataSource={warmupList.map(w => w.name)}
-            maxSearchResults={3}
-            fullWidth
-            onUpdateInput={text => this.setState({warmupFilterText: text})}
-          />
 
-          <Select
-            name="tag-search-field"
-            placeholder="Filter by tags"
-            value={this.state.selectedFilterTags}
-            onChange={this.tagSearchChanged}
-            multi={true}
-            options={Array.from(tags).map(t => {
-              return {value: t, label: t};
-            })}
-          />
-        </div>
+        { this.renderFilters() }
 
         <div className="warmups-list">
           <List>
@@ -93,18 +124,11 @@ class App extends Component {
           </List>
         </div>
       </div>
-      <div className="view-source">
-        <img src={githubIcon} className="github-icon" alt="github icon"/>
-        <a
-          href="https://github.com/evykassirer/choir-warmups"
-          className="view-source-link">
-          View source code on GitHub
-        </a>
-      </div>
+
+      { this.renderViewSource() }
     </MuiThemeProvider>;
   }
 }
-
 
 
 export default App;
