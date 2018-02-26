63a1e4778a6bb8bb64cc4490741d4128337a4d53
evykassirer <evy.kassirer@gmail.com>
2018-02-25 21:28:52 -0500

    exercise stuff

--- src/WarmupList.js
+++ src/WarmupList.js
@@ -3,15 +3,19 @@ import { List } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 import AutoComplete from 'material-ui/AutoComplete';
 import Select from 'react-select';
+import Toggle from 'material-ui/Toggle';
 import 'react-select/dist/react-select.css';
 
 import Warmup from './Warmup.js';
 import warmups from './warmups.json';
+import exercises from './exercises.json';
 
 class WarmupList extends Component {
 	state = {
 	  warmupFilterText: "",
 	  selectedFilterTags: [],
+	  showExercises: false,
+	  showWarmups: true,
 	}
 
 
@@ -20,9 +24,22 @@ class WarmupList extends Component {
 	}
 
 
+	toggleExercises = (event, isInputChecked) => {
+		this.setState({showExercises: isInputChecked});
+	}
+
+	toggleWarmups = (event, isInputChecked) => {
+		this.setState({showWarmups: isInputChecked});
+	}
+
+
 	sortedWarmupList() {
 	  const warmupList = warmups["warmups"];
-	  warmupList.sort(function(a, b) {
+	  warmupList.forEach(warmup => warmup["isExercise"] = false);
+	  const exerciseList = exercises["exercises"];
+	  exerciseList.forEach(exercise => exercise["isExercise"] = true);
+	  const list = warmupList.concat(exerciseList);
+	  list.sort(function(a, b) {
 	      if (a.name.toLowerCase() < b.name.toLowerCase()) {
 	        return -1;
 	      }
@@ -31,7 +48,7 @@ class WarmupList extends Component {
 	      }
 	      return 0;
 	  });
-	  return warmupList;
+	  return list;
 	}
 
 
@@ -45,6 +62,18 @@ class WarmupList extends Component {
 	      if (!warmup.tags.includes(tag.value)) {
 	        return false;
 	      }
+	    }
+
+	    if (!this.state.showExercises) {
+	    	if (warmup.isExercise) {
+	    		return false;
+	    	}
+	    }
+
+	    if (!this.state.showWarmups) {
+	    	if (!warmup.isExercise) {
+	    		return false;
+	    	}
 	    }
 
 	    return true;
@@ -65,7 +94,7 @@ class WarmupList extends Component {
 	renderFilters() {
 	  return <div className="warmups-search">
 	    <AutoComplete
-	      floatingLabelText="Search by warmup name"
+	      floatingLabelText="Search by name"
 	      filter={AutoComplete.fuzzyFilter}
 	      dataSource={this.sortedWarmupList().map(w => w.name)}
 	      maxSearchResults={3}
@@ -83,6 +112,22 @@ class WarmupList extends Component {
 	        return {value: t, label: t};
 	      })}
 	    />
+
+	    <div className="warmupExercisetoggles">
+		    <Toggle
+	        label="Show warmups"
+	        toggled={this.state.showWarmups}
+		    	onToggle={this.toggleWarmups}
+	      />
+  	    <Toggle
+          label="Show exercises"
+          toggled={this.state.showExercises}
+  	    	onToggle={this.toggleExercises}
+        />
+	    </div>
+	    <div className="exerciseHelpText">
+	      (exercises are used to teach in rehearsal instead of warmup time)
+	     </div>
 	  </div>;
 	}
 
